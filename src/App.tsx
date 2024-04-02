2ad4ff31a406f373be6106cacbe5d7813a1180ec
Mauri <mauri.monzon91@gmail.com>
2024-04-02 09:46:20 -0300

    Base

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {StatusBar, Text, View} from 'react-native';
+import {CalculatorScreen} from './presentation/screens/CalculatorScreen';
+
+function App() {
+  return (
+    <View>
+      <StatusBar barStyle={'light-content'} backgroundColor={'black'} />
+      <CalculatorScreen />
+    </View>
+  );
+}
+
+export default App;
